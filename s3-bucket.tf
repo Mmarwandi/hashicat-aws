4d69d79dcf311a831e02af6af8b03abf9a1a2b24
Marwandi M <marwandi.marwandi@gmail.com>
2021-10-13 04:35:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,8 @@ module "s3_bucket" {
   source = "app.terraform.io/datatective-ml/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "${var.prefix}"
+  bucket = "my-s3-bucket"
+  bucket_prefix = "${var.prefix}"
   acl    = "private"
 
   versioning = {
